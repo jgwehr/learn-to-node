0fd446990d93321705d8dcf605482b54d6fd7a8a
jgwehr <dm.nota@gmail.com>
2018-04-27 22:10:36 -0500

    Clearer Server setup

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+var app = express();
+app
   .use(express.static(path.join(__dirname, 'public')))
+  .get('/', function (req, res) {
+    res.send('Hello World')
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
