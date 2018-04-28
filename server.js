5deaa0cfc116e19d71d73b45ebe19aedaf309dad
jgwehr <dm.nota@gmail.com>
2018-04-27 22:20:16 -0500

    putpostdelete

--- server.js
+++ server.js
@@ -8,4 +8,16 @@ app
   .get('/', function (req, res) {
     res.send('Hello World')
   })
+
+  .post('/', function (req, res) {
+    res.send('Got a POST request')
+  })
+  .put('/', function (req, res) {
+    res.send('Got a PUT request at /user')
+  })
+  .delete('/', function (req, res) {
+    res.send('Got a DELETE request at /user')
+  })
+
+
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
