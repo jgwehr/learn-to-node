f33f97e276e8f5d14f13292bf81a90cd29c15ae4
Joe <jgwehr@users.noreply.github.com>
2018-04-27 23:06:04 -0500

    Implemented express.route()
    https://expressjs.com/en/guide/routing.html

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 var app = express();
-app
-  .use(express.static(path.join(__dirname, 'public')))
-  .get('/', function (req, res) {
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.route('/')
+  .get(function (req, res) {
     res.send('Hello World')
   })
-
-  .post('/', function (req, res) {
+  .post(function (req, res) {
     res.send('Got a POST request')
   })
-  .put('/', function (req, res) {
+  .put(function (req, res) {
     res.send('Got a PUT request at /user')
   })
-  .delete('/', function (req, res) {
+  .delete(function (req, res) {
     res.send('Got a DELETE request at /user')
   })
 
 
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 console.log(`NODE_ENV=${process.env.NODE_ENV}`);
